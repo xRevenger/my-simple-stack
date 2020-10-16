1676ceddee0aa9c1c80dcdf9821f2b96cb2ca377
xRevenger <chukewin@gmail.com>
2020-10-16 09:40:05 +0200

    TP2 Exercice 3

---
+++ src/main/java/deqo/kchu/mysimplestack/Item.java
@@ -0,0 +1,91 @@
+package deqo.kchu.mysimplestack;
+
+import java.util.EmptyStackException;
+
+public class Item implements ISimpleStack{
+    private int valeur;
+    private Item suivant;
+    private Boolean vide;
+
+    public Item(int val) {
+        this.valeur = val;
+        this.suivant = null;
+    }
+
+    public Item(Boolean vide) {
+        if (vide) {
+            this.valeur = -1;
+            this.suivant = null;
+            this.vide = true;
+        }
+    }
+
+    public int getVal() {
+        return this.valeur;
+    }
+
+    public Item getSuivant() {
+        return this.suivant;
+    }
+
+    public void setSuivant(Item item) {
+        this.vide = false;
+        this.suivant = item;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (this.vide);
+    }
+
+    @Override
+    public int getSize() {
+        Item itCourant = this;
+        int i = 0;
+        while(itCourant != null) {
+            i++;
+            itCourant = itCourant.getSuivant();
+        }
+        return i-1;
+    }
+
+    @Override
+    public void push(Item item) {
+        Item itCourant = this;
+        while(itCourant.getSuivant() != null) {
+            itCourant = itCourant.getSuivant();
+        }
+        itCourant.setSuivant(item);
+    }
+
+    @Override
+    public Item peek() throws EmptyStackException {
+        if (this.vide)
+            throw new EmptyStackException();
+
+        Item itCourant = this;
+        while(itCourant.getSuivant() != null) {
+            itCourant = itCourant.getSuivant();
+        }
+        return itCourant;
+    }
+
+    @Override
+    public Item pop() throws EmptyStackException {
+        if (this.vide)
+            throw new EmptyStackException();
+
+        Item itCourant = this;
+        Item itPrecedent = new Item(true);
+        while(itCourant.getSuivant() != null) {
+            itPrecedent = itCourant;
+            itCourant = itCourant.getSuivant();
+        }
+        itPrecedent.setSuivant(null);
+
+        if (itPrecedent.getVal() == -1)
+            this.vide = true;
+
+        return itCourant;
+    }
+}
---
+++ src/test/java/deqo/kchu/mysimplestack/ItemTest.java
@@ -0,0 +1,40 @@
+package deqo.kchu.mysimplestack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    @Test
+    public void getVal() {
+    }
+
+    @Test
+    public void getSuivant() {
+    }
+
+    @Test
+    public void setSuivant() {
+    }
+
+    @Test
+    public void isEmpty() {
+    }
+
+    @Test
+    public void getSize() {
+    }
+
+    @Test
+    public void push() {
+    }
+
+    @Test
+    public void peek() {
+    }
+
+    @Test
+    public void pop() {
+    }
+}
