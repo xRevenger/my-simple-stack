b45ac05c43f0cdbf76e84410cc053a0fd15ec32b
xRevenger <chukewin@gmail.com>
2020-10-16 09:17:27 +0200

    TP2 Exercice 2.3

---
+++ src/main/java/deqo/kchu/mysimplestack/App.java
@@ -0,0 +1,13 @@
+package deqo.kchu.mysimplestack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
